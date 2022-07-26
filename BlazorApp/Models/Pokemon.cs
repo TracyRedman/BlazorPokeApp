9d1d7349a6abc72e1405102da23898653d4523b2
Tracy Redman <redmanatracy@gmail.com>
2022-07-26 19:35:43 -0400

    init commit

---
+++ BlazorApp/Models/Pokemon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace BlazorApp.Models
+{
+    public class Pokemon
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("weight")]
+        public int Weight { get; set; }
+        [JsonProperty("height")]
+        public int Height { get; set; }
+        [JsonProperty("base_experience")]
+        public int Experience { get; set; }
+    }
+}
+
